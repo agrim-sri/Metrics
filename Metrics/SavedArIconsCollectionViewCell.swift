0ef1e4ad51c1db23b16fe8ae589cc428c1f1ef53
Devjyoti Mohanty <68328634+Dev26112001@users.noreply.github.com>
2023-03-03 18:37:34 +0530

    savedaricons

---
+++ Metrics/SavedArIconsCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DesignCollectionViewCell.swift
+//  Metrics
+//
+//  Created by Devjyoti Mohanty on 03/03/23.
+//
+
+import UIKit
+
+class SavedArIconsCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var savedImageView: UIImageView!
+    
+    
+    @IBOutlet weak var titleLbl: UILabel!
+    
+    
+    
+}
---
+++ Metrics/SavediconsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SavediconsViewController.swift
+//  Metrics
+//
+//  Created by Devjyoti Mohanty on 03/03/23.
+//
+
+import UIKit
+
+class SavediconsViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
